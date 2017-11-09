17ec97ada0112dc45030b858e5c81017e0ae059d
Arjun Kakkar <ak23@swy-241-01m.williams.edu>
2017-11-09 14:49:14 -0500

    Updates

--- example/energyminimizer_new.m
+++ example/energyminimizer_new.m
@@ -1,17 +1,19 @@
 %Define some parameters
-xmin=0; xmax=30;   % Determines the x limits of the region
-ymin=0; ymax=30;   % Determines the y limits of the region
-xnum=120; ynum=120; % Determines the amount of refinement required
+xmin=0; xmax=100;   % Determines the x limits of the region
+ymin=0; ymax=100;   % Determines the y limits of the region
+xnum=100; ynum=100; % Determines the amount of refinement required
 
 xinc = (xmax-xmin)/xnum; % Calculates length of x interval
 yinc = (ymax-ymin)/ynum; % Calculates length of y interval
 
 % Define the initial grid
- conc = repmat(sin(pi*[1:xnum]/xnum).^2, ynum, 1);
+% conc = repmat(sin(2*pi*[1:xnum]/xnum).^2, ynum, 1);
+conc = sin(pi*[1:xnum]/xnum)' * sin(pi*[1:ynum]/ynum);
 % conc = rand(xnum,ynum)-0.5;
+% conc = repmat(exp([1:xnum]./xnum), ynum, 1);
 
 % Set the value of constants
-constants = [1, 1, .1];
+constants = [1, 1, 0.05];
 
 % Define anonymous function to be minimized
 minfun = @(X) newregion_opt(X, constants, xinc, yinc);
@@ -32,7 +34,11 @@ options = optimoptions('fmincon', 'MaxFunctionEvaluations', 200000,...
 % The constraint on the solution is that it should be greater than 0
 
 % Generate a plot of the concentration in the region
+subplot(2, 1, 1)
  xgrid = xmin+xinc/2:xinc:xmax-xinc/2;
  ygrid = ymin+yinc/2:yinc:ymax-yinc/2;
  surf(xgrid, ygrid, minconc)
  title('Surface Plot of the Minimizing Concentrations')
+subplot(2, 1, 2)
+ surf(xgrid, ygrid, conc)
+ title('Surface Plot of the Initial Concentrations')
