9360c07a6b94b28bef93ea911d25f1d7a0aecf45
Arjun Kakkar <arjunkakkar8@gmail.com>
2017-11-27 14:13:36 -0500

    Some updates

--- simplemodel2/constraint.m
+++ simplemodel2/constraint.m
@@ -2,30 +2,13 @@ function [c,ceq] = constraint(conc, g, constants, xinc, yinc, xnum, ynum)
 % Constants corresponding to stored values in the constants array
 c0 = constants(1);
 c1 = constants(2);
-% The five required values from the matrix
-% u12 = circshift(conc, [1,0]);
-% u32 = circshift(conc, [-1,0]);
-% u21 = circshift(conc, [0,1]);
-% u23 = circshift(conc, [0,-1]);
-% Energy equation from the proposed model
-%e = conc.*(-c2.*((u32-u12).^2)/(4*yinc.^2)-c2.*((u23-u21).^2)/(4*xinc.^2)+c0 - c1.*conc-c3.*g);
-%ceq = conc.*(-c2.*((u32-u12).^2)/(4*yinc.^2)-c2.*((u23-u21).^2)/(4*xinc.^2)+c0*(ynum-repmat(1:ynum, [xnum,1]))-c1*cumsum(conc, 2, 'reverse'));
-ceq = conc.*(c0-c1.*conc-water(conc, constants, xnum, ynum));
-%ceq = conc.*(c0-c1.*conc);
-c = [];
-end
-
-function [aw] = water(conc, constants, xnum, ynum)
 c2 = constants(3);
 c3 = constants(4);
-precipitation = c2.*ones(xnum, ynum);
-flow = precipitation - c3.*conc;
-for i=0:ynum-2
-    nonzero = flow(:,ynum-i)>0;
-    flow(:,ynum-i)=flow(:,ynum-i).*nonzero;
-    flow(:,ynum-i-1) = flow(:,ynum-i-1)+flow(:,ynum-i);
-end
-nonzero = flow(:,1)>0;
-flow(:,1)=flow(:,1).*nonzero;
-aw=flow;
+
+w = c2.*ones(xnum, ynum);
+p = c3.*conc;
+%water = max(w - p + max(cumsum(w-p, 2,'reverse'),0),0);
+water = cumsum(w-p, 2,'reverse');
+ceq = conc.*(c0-c1.*conc-water);
+c = -water;
 end
--- simplemodel2/minimizer.m
+++ simplemodel2/minimizer.m
@@ -1,23 +1,29 @@
+function[minconc, minenergy, process] = minimizer(a)
+
+process = [];
+
 %Define some parameters
 xmin=0; xmax=100;   % Determines the x limits of the region
 ymin=0; ymax=100;   % Determines the y limits of the region
-xnum=30; ynum=30; % Determines the amount of refinement required
+xnum=20; ynum=20; % Determines the amount of refinement required
 
 xinc = (xmax-xmin)/xnum; % Calculates length of x interval
 yinc = (ymax-ymin)/ynum; % Calculates length of y interval
 
 % Define the initial grid
 % conc = repmat(sin(2*pi*[1:xnum]/xnum).^2, ynum, 1)+0.1*rand(xnum,ynum);
- conc = sin(1*pi*[1:xnum]/xnum).^2' * sin(3*pi*[1:ynum]/ynum).^2+0.1*rand(xnum,ynum);
+% conc = 0.5 * sin(1*pi*[1:xnum]/xnum).^2' * sin(3*pi*[1:ynum]/ynum).^2+0.1*rand(xnum,ynum);
 
-% conc = rand(xnum,ynum);
+%conc = ones(xnum, ynum);
+
+ conc = rand(xnum,ynum);
 % conc = repmat(exp([1:xnum]./xnum), ynum, 1);
 
 % Define the landscape
 g = repmat([1:xnum]./xnum, ynum, 1);
 
 % Set the value of constants
-constants = [1, 1, 1, 3];
+constants = [1, 1, 1, 5];
 
 % Define anonymous function to be minimized
 %objfun = @(X) objfun(X);
@@ -25,15 +31,15 @@ constrfun = @(Y) constraint(Y, g, constants, xinc, yinc, xnum, ynum);
 
 % Set the number of iterations for the optimizer
 options = optimoptions('fmincon', 'MaxFunctionEvaluations', 300000,...
-    'Hessian', {'lbfgs',30}, 'TolCon', 1e-5,'TolFun',1e-5,'TolX',1e-5,...
+    'Hessian', {'lbfgs',30}, 'TolCon', 1e-8,'TolFun',1e-8,'TolX',1e-8,...
     'UseParallel', true, 'SpecifyObjectiveGradient',true,...
-    'Display', 'iter', 'Algorithm', 'interior-point',...
-    'PlotFcn', @show);
+    'Display', 'iter', 'Algorithm', 'sqp',...
+    'OutputFcn', @outfun);
 
 
 
 % Run optimizer using the prescribed options
-[minconc, minenergy] = fmincon('objfun', conc,[],[],[],[],conc*0,[], constrfun, options)
+[minconc, minenergy] = fmincon('objfun', conc,[],[],[],[],conc*0,[], constrfun, options);
 
 % Generate a plot of the concentration in the region
 subplot(2, 2, [1,2])
@@ -47,8 +53,10 @@ subplot(2, 2, 3)
 subplot(2, 2, 4)
  surf(xgrid, ygrid, g)
  title('Landscape')
- 
- 
-function a = show(x, xgrid, ygrid)
-a=surf(xgrid, ygrid, x)
+
+    function stop = outfun(x,optimValues,state)
+        stop = false;
+        process = [process; x];
+    end
+
 end
