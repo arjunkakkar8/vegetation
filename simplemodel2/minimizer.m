8d2ae94a0005aa2cc8c746d5988351d0c14c1f01
Arjun Kakkar <arjunkakkar8@gmail.com>
2017-12-11 15:20:06 -0500

    Stuff

--- simplemodel2/minimizer.m
+++ simplemodel2/minimizer.m
@@ -5,7 +5,7 @@ process = [];
 %Define some parameters
 xmin=0; xmax=100;   % Determines the x limits of the region
 ymin=0; ymax=100;   % Determines the y limits of the region
-xnum=10; ynum=10; % Determines the amount of refinement required
+xnum=10; ynum=2; % Determines the amount of refinement required
 
 xinc = (xmax-xmin)/xnum; % Calculates length of x interval
 yinc = (ymax-ymin)/ynum; % Calculates length of y interval
@@ -14,16 +14,16 @@ yinc = (ymax-ymin)/ynum; % Calculates length of y interval
 % conc = repmat(sin(2*pi*[1:xnum]/xnum).^2, ynum, 1)+0.1*rand(xnum,ynum);
 
 % conc =  sin(1*pi*[1:xnum]/xnum).^2' * sin(3*pi*[1:ynum]/ynum).^2+0.1*rand(xnum,ynum);
-conc =  sin(2*pi*[1:xnum]/xnum).^8' * sin(1*pi*[1:ynum]/ynum).^8+0.1*rand(xnum,ynum);
+%conc =  sin(2*pi*[1:xnum]/xnum).^2' * sin(1*pi*[1:ynum]/ynum).^2+0.1*rand(xnum,ynum);
 % conc = ones(xnum, ynum);
-% conc = rand(xnum,ynum);
+ conc = rand(xnum,ynum);
 % conc = repmat(exp([1:xnum]./xnum), ynum, 1);
 
 % Define the landscape
 g = repmat([1:xnum]./xnum, ynum, 1);
 
 % Set the value of constants
-constants = [1, 1, 1, 3, -30];
+constants = [1, 1, 1, 3, 5];
 
 % Define anonymous function to be minimized
 %objfun = @(X) objfun(X);
@@ -40,15 +40,13 @@ options = optimoptions('fmincon', 'MaxFunctionEvaluations', 60000,...
 
 % Generate a plot of the concentration in the region
 subplot(2, 2, [1,2])
- xgrid = xmin+xinc/2:xinc:xmax-xinc/2;
- ygrid = ymin+yinc/2:yinc:ymax-yinc/2;
- surf(xgrid, ygrid, minconc)
+ surf(minconc)
  title('Surface Plot of the Minimizing Concentrations')
 subplot(2, 2, 3)
- surf(xgrid, ygrid, conc)
+ surf(conc)
  title('Surface Plot of the Initial Concentrations')
 subplot(2, 2, 4)
- surf(xgrid, ygrid, g)
+ surf(g)
  title('Landscape')
 
     function stop = outfun(x,optimValues,state)
