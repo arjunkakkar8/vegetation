ea59b963bf4c5f4932c42286e3deae53c7f372d6
Arjun Kakkar <ak23@swy-241-02m.williams.edu>
2017-12-11 15:18:21 -0500

    Changes

--- simplemodel2/animator.m
+++ simplemodel2/animator.m
@@ -1,6 +1,6 @@
 history = process;
-for k = 1:size(history, 1)/20-1
-    pause(1)
-    surf(history(k*20:k*20+20,:))
+for k = 1:size(history, 1)/3-1
+    pause(0.5)
+    surf(history(k*3:k*3+3,:))
     drawnow
 end
